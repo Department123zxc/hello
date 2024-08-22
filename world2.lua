663c9425678edcd5d8dd7a355a1d568902c9fb87
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2024-08-22 08:36:09 +0700

    Update world2.lua

--- world2.lua
+++ world2.lua
@@ -6,7 +6,7 @@ return {
 4071152810,
 856191260,
 3066114825, -- Dương 1
-1161468792, -- Minh Phạm
+--1161468792, -- Minh Phạm
 0, -- Gia Khánh
 5089621002, -- Trần Huy Quang
 0, -- Nguyễn Văn Bảo Khánh
