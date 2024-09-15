58d54151ca4f6d6c6d63906469c6ce14ecdd32db
PotatoNotTomato <140255333+PotatoEiii@users.noreply.github.com>
2024-09-15 19:39:57 +0700

    Update world2.lua

--- world2.lua
+++ world2.lua
@@ -12,7 +12,7 @@ return {
 0, -- Trần Huy Quang
 0, -- Nguyễn Văn Bảo Khánh
 0, -- Bùi Đức An
-0, -- Hiếu Trung
+1141261219, -- Hiếu Trung
 3966009046, -- Đào Hải Sơn
 3528921703, -- Lê Trường Giang
 0, -- Gia Khánh
